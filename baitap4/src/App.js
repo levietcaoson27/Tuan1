5a7b12f50603d2faed32c5b35818ee842c22eb5f
levietcaoson <levietcaoson27@gmail.com>
2026-01-18 00:36:16 +0700

    add

---
+++ baitap4/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginForm from './components/LoginForm/LoginForm';
+
+function App() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
